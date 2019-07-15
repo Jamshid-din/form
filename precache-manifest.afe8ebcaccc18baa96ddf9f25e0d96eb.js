1d1f772e0aca15f5dbbad519c08f7d4643faea16
Jamshid Ismailov <jamshid@Jamshids-MacBook-Pro.local>
2019-07-16 00:15:53 +0500

    Updates

---
+++ precache-manifest.afe8ebcaccc18baa96ddf9f25e0d96eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81aa3c8f150c3e9bf68f8b924ada9f10",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "255902f18e7d0fb620c3",
+    "url": "/form/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bd469d8a6d36a32fc893",
+    "url": "/form/static/css/main.dc7e40eb.chunk.css"
+  },
+  {
+    "revision": "255902f18e7d0fb620c3",
+    "url": "/form/static/js/2.947e8d9e.chunk.js"
+  },
+  {
+    "revision": "bd469d8a6d36a32fc893",
+    "url": "/form/static/js/main.55fbe9c1.chunk.js"
+  },
+  {
+    "revision": "6aa6fedf41f3d1e963bb",
+    "url": "/form/static/js/runtime~main.2305f2a1.js"
+  }
+]);
